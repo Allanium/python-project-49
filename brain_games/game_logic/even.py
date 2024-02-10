22adb0479cd0d2de0132a66fbf30985b37f6c2a4
Владислав Грищенко <wvrpystudio@Wvrpy-Studo.local>
2024-02-10 23:38:33 +0300

    fix

---
+++ brain_games/game_logic/even.py
@@ -0,0 +1,13 @@
+from random import randint
+
+DESCRIPTION = 'Answer "yes" if the number is even, otherwise answer "no".'
+
+
+def is_even():
+    number = randint(1, 100)
+    question = f'{number}'
+    if number % 2 == 0:
+        answer = 'yes'
+    else:
+        answer = 'no'
+    return question, answer
---
+++ brain_games/scripts/brain_even.py
@@ -0,0 +1,11 @@
+from brain_games import main
+from random import randint
+
+
+name = main()
+print('Answer "yes" if the number is even, otherwise answer "no".'
+
+
+def is_even(number):
+    
+        
