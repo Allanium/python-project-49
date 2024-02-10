8d68c169048264c8f3f619638a99b11121a124fb
Allanium <88595135+Allanium@users.noreply.github.com>
2024-02-11 00:32:43 +0300

    Update game.py

--- brain_games/game.py
+++ brain_games/game.py
@@ -7,7 +7,7 @@ def game(some_game):
     name = string('May I have your name? ')
     print(f'Hello, {name}!')
     print({logic.DESCRIPTION})
-    from i in range(ROUNDS):
+    for i in range(ROUNDS):
         question, answer = logic.get_round()
         print(f'Question: {question}')
         user_answer = string('Your answer:  ')
