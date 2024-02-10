48ee9bd2cff6879651cb07c13f2d66484e5da9f6
Владислав Грищенко <wvrpystudio@Wvrpy-Studo.local>
2024-02-11 00:46:13 +0300

    fix

--- brain_games/game_logic/cli.py
+++
@@ -1,9 +0,0 @@
-import prompt
-
-def start_game():
-    print('Welcome to the Brain Games!')
-    return prompt.string('May I have your name? ')
-
-
-def hello_user(name):
-    print(f'Hello, {name}!'    
