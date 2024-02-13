dfd4cb7de5570a0375d70c19b970b7c0b2f32be7
Allanium <88595135+Allanium@users.noreply.github.com>
2024-02-14 02:12:05 +0300

    Update progression.py

--- brain_games/game_logic/progression.py
+++ brain_games/game_logic/progression.py
@@ -1,1 +1,21 @@
+from random import randint
+
+DESCRIPTION = 'What number is missing in the progression?'
+
+def get_round():
+    start_progression = randint(1, 100)
+    step = randint(2, 5)
+    number_of_digits = randint(5, 10)
+    end_progression = start_progression + step * number_of_digits
+    progression = list(range(start_progression, end_progression, step))
+    randon_number = randint(0, number_of_digits)
+    answer = str(progression[random_number])
+    progression[random_number] = '..'
+    question = ''
+    for number in progression:
+        question += str(number) + ' '
+    question = str(question.strip())
+    return question, answer
+
+
 
