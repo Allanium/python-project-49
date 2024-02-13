7faccfa4057fb8c27a0a6d888baab6ef44a829d6
Allanium <88595135+Allanium@users.noreply.github.com>
2024-02-14 01:57:05 +0300

    Update brain_progression.py

--- brain_games/scripts/brain_progression.py
+++ brain_games/scripts/brain_progression.py
@@ -1,1 +1,12 @@
+# !/usr/bin/env python
 
+from brain_games.game import game
+from brain_games.game_logic import progression
+
+
+def main():
+    game(progression)
+
+
+if __name__ == '__main__':
+    main()
