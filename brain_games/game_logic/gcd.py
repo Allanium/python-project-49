76df65f289340c086c177acdf437de2b0798d267
Allanium <88595135+Allanium@users.noreply.github.com>
2024-02-14 22:51:44 +0300

    Update gcd.py

--- brain_games/game_logic/gcd.py
+++ brain_games/game_logic/gcd.py
@@ -3,9 +3,10 @@ from random import randint
 
 DESCRIPTION = 'Find the greatest common divisor of given numbers.'
 
+
 def get_round():
-    num1 = randint(1,100)
-    num2 = randint(1,100)
+    num1 = randint(1, 100)
+    num2 = randint(1, 100)
     question = f'{num1} {num2}'
     answer = str(gcd(num1, num2))
     return question, answer
