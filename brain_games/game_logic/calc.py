c9e0c4b53834ea45d0e04c94126c51c3711d8aa0
Allanium <88595135+Allanium@users.noreply.github.com>
2024-02-14 23:27:39 +0300

    Update calc.py

--- brain_games/game_logic/calc.py
+++ brain_games/game_logic/calc.py
@@ -7,14 +7,14 @@ def get_round():
     num1 = randint(1, 100)
     num2 = randint(1, 100)
     operator = choice('+', '-', '*')
-    
+
     if operator == '+': 
         answer = num1 + num2
     elif operator == '-':
         answer = num1 - num2
     else:
         answer = num1 * num2
-    
+
     question = f'{num1} {operator} {num2}'
 
     return question, answer
