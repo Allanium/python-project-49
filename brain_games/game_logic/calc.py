db1500bc9d6cd1a6b14e6061951caa06b2808bc8
Allanium <88595135+Allanium@users.noreply.github.com>
2024-02-14 23:53:57 +0300

    Update calc.py

--- brain_games/game_logic/calc.py
+++ brain_games/game_logic/calc.py
@@ -6,7 +6,7 @@ DESCRIPTION = 'What is the result of the expression?'
 def get_round():
     num1 = randint(1, 100)
     num2 = randint(1, 100)
-    operator = choice('+', '-', '*')
+    operator = choice(['+', '-', '*'])
 
     if operator == '+':
         answer = num1 + num2
