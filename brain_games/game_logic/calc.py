f328731a93f68e505865b3ca156076dec8dadb8a
Allanium <88595135+Allanium@users.noreply.github.com>
2024-02-14 23:23:11 +0300

    Update calc.py

--- brain_games/game_logic/calc.py
+++ brain_games/game_logic/calc.py
@@ -6,15 +6,15 @@ DESCRIPTION = 'What is the result of the expression?'
 def get_round():
     num1 = randint(1, 100)
     num2 = randint(1, 100)
-
-    answer = {
-        '+': num1 + num2,
-        '-': num1 - num2,
-        '*': num1 * num2
-    }
-
-    random_key = choice(list(answer.keys()))
-    answer = str(answer[random_key])
-    question = f'{num1} {random_key} {num2}'
+    operator = choice('+', '-', '*')
+    
+    if operator == '+': 
+        answer = num1 + num2
+    elif operator == '-':
+        answer = num1 - num2
+    else:
+        answer = num1 * num2
+    
+    question = f'{num1} {operator} {num2}'
 
     return question, answer
