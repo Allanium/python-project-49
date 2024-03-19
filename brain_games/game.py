04d08364c536590e0010320904bb98695ed0f330
Allanium <88595135+Allanium@users.noreply.github.com>
2024-03-19 15:14:59 +0300

    Update game.py

--- brain_games/game.py
+++ brain_games/game.py
@@ -8,10 +8,12 @@ def game(some_game):
     name = string('May I have your name? ')
     print(f'Hello, {name}!')
     print(some_game.DESCRIPTION)
+    
     for _ in range(ROUND):
         question, answer = some_game.get_round()
         print(f'Question: {question}')
         user_answer = string('Your answer:  ')
+        
         if user_answer != answer:
             print(f"'{user_answer}' is wrong answer ;(. "
                   f"Correct answer was '{answer}'\n"
