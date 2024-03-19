488bc0c42128d010370a7f2ac72ebc8455891be3
Allanium <88595135+Allanium@users.noreply.github.com>
2024-03-19 15:11:27 +0300

    Update game.py

--- brain_games/game.py
+++ brain_games/game.py
@@ -1,6 +1,6 @@
 from prompt import string
 
-ROUNDS = 3
+ROUND = 3
 
 
 def game(some_game):
@@ -8,7 +8,7 @@ def game(some_game):
     name = string('May I have your name? ')
     print(f'Hello, {name}!')
     print(some_game.DESCRIPTION)
-    for i in range(ROUNDS):
+    for i in range(ROUND):
         question, answer = some_game.get_round()
         print(f'Question: {question}')
         user_answer = string('Your answer:  ')
